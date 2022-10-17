1510d662743150b8da93528de4643a11c28aaa6d
Efraim Kristhianno <ekriator@gmail.com>
2022-10-17 16:35:48 -0400

    atualizando

--- main.py
+++ main.py
@@ -1,34 +1,36 @@
-from flask import Flask, jsonify, make_response, request
+from flask import Flask, jsonify, request
 
 from bdFake import Clientes
 
-app = Flask(__name__)  # instancia do Flask
-
-
-# por padrão o Flask ordena os dados de A-Z.
+app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 
 
-# decorator do flask para indicar que a função criada é uma requisição do usuario portanto uma rota que precisa ser executada com o método GET.
 @app.route('/Clientes', methods=['GET'])
-def pegar_Clientes():  # função que retorna a lista JSON de todos os clientes cadastrados.
-    # configurando a forma como esta lista será apresentada ao usuário.
-    return make_response(jsonify(Clientes))
+def return_AllClientes():
+    return jsonify({'Clientes': Clientes})
 
 
-# decorator do flask para indicar que a função criada é uma requisição do usuario portanto uma rota que precisa ser executada com o método POST.
+@app.route('/Clientes/<string:nome>', methods=['GET'])
+def return_OneCliente(nome):
+    OneCliente = [Cliente for Cliente in Clientes if Cliente['nome'] == nome]
+    return jsonify({'Cliente': OneCliente[0]})
+
+
 @app.route('/Clientes', methods=['POST'])
-def criar_Cliente():  # função que cria um novo Cliente.
-    Cliente = request.json  # O arquivo enviado ao banco de dados será do tipo .json
-    # Chamando o Banco de dados para adicionar o novo dado em seu conjunto de dados 'lista'
+def create_Cliente():
+    Cliente = {'id': request.json['id'],
+               'nome': request.json['nome'],
+               'profissao': request.json['profissao'],
+               'idade': request.json['idade'],
+               'email': request.json['email']
+               }
     Clientes.append(Cliente)
-    # Retornando a nova lista de Clientes atualizadas depois de adicionar mais um cliente.
-    return make_response(
-        jsonify(
-            mensagem='Lista de Clientes',
-            dados=Clientes
-        )
-    )
+    return jsonify({'Lista de Clientes': Clientes})
 
 
-app.run()  # executa a API Rest
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
+
+
+# app.run()
