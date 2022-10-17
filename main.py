30f2921a2ca8a47e33c24566d4ca54e44ed5a270
Efraim Kristhianno <ekriator@gmail.com>
2022-10-17 17:55:08 -0400

    add files

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 
 
-@app.route('/Clientes', methods=['GET'])
+@app.route('/', methods=['GET'])
 def return_AllClientes():
     return jsonify({'Clientes': Clientes})
 
 
-@app.route('/Clientes/<string:nome>', methods=['GET'])
+@app.route('/<string:nome>', methods=['GET'])
 def return_OneCliente(nome):
     OneCliente = [Cliente for Cliente in Clientes if Cliente['nome'] == nome]
     return jsonify({'Cliente': OneCliente[0]})
 
 
-@app.route('/Clientes', methods=['POST'])
+@app.route('/', methods=['POST'])
 def create_Cliente():
     Cliente = {'id': request.json['id'],
                'nome': request.json['nome'],
@@ -29,7 +29,7 @@ def create_Cliente():
     return jsonify({'Lista de Clientes': Clientes})
 
 
-@app.route('/Clientes/<string:profissao>', methods=['PUT'])
+@app.route('/<string:profissao>', methods=['PUT'])
 def update_Cliente(profissao):
     UpdateCliente = [
         Cliente for Cliente in Clientes if Cliente['profissao'] == profissao]
@@ -37,7 +37,7 @@ def update_Cliente(profissao):
     return jsonify({'Clientes': UpdateCliente[0]})
 
 
-@app.route('/Clientes/<string:nome>', methods=['DELETE'])
+@app.route('/<string:nome>', methods=['DELETE'])
 def delet_Cliente(nome):
     delCliente = [Cliente for Cliente in Clientes if Cliente['nome'] == nome]
     Clientes.remove(delCliente[0])
@@ -46,6 +46,3 @@ def delet_Cliente(nome):
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
-
-
-# app.run()
